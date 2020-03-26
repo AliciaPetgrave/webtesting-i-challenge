62b79ac0c3edbaee353af78edf856571e9fc8e5c
Alicia Petgrave <alicia.petgrave@gmail.com>
2020-03-26 13:58:55 -0400

    building out methods and adding tests

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,46 @@ module.exports = {
   get,
 };
 
+  // name
+  // durability 0 - 100
+  // enhancement 0 - 20
+
 function succeed(item) {
+  const {enhancement} = item
+  if (enhancement <= 19){
+    return {
+      ...item,
+      enhancement: enhancement + 1}
+  } else
   return { ...item };
 }
 
 function fail(item) {
+  const {durability, enhancement} = item
+  if (enhancement < 15){
+    return {
+      ...item,
+      durability: durability - 5}
+  } else if (enhancement > 16){
+    return {
+      ...item,
+      durability: durability - 1}
+  } else if (enhancement >= 15){
+    return {
+      ...item,
+      durability: durability - 10}
+  } else
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  if (item.durability <= 100){
+    return {
+      ...item,
+    durability: 100
+    }
+  };
+  return item
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,38 @@
-const enhancer = require('./enhancer.js');
+const {repair, succeed, fail, get} = require('./enhancer.js');
+
 // test away!
+it("should run the tests", function() {
+    expect(true).toBe(true);
+  });
+
+describe('enhancer.js', function(){
+
+    //repair
+    describe('.repair()', () => {
+        it("should increase durability to 100", () => {
+            expect(repair({ durability: 10 }).durability).toBe(100)
+            expect(repair({ durability: 19 }).durability).toBe(100)
+            expect(repair({ durability: 100 }).durability).toBe(100)
+        })
+    })
+
+    //succeed
+    describe('.succeed()', () => {
+        it('should increase enhancement by 1', () => {
+            expect(succeed({ enhancement: 10 }).enhancement).toBe(11)
+            expect(succeed({ enhancement: 19 }).enhancement).toBe(20)
+            expect(succeed({ enhancement: 20 }).enhancement).toBe(20)
+        })
+    })
+
+    //fail
+    describe('.fail()', () => {
+        it('should change enhancement', () => {
+            expect(fail({ enhancement: 10, durability: 10 }).durability).toBe(5)
+            expect(fail({ enhancement: 15, durability: 20 }).durability).toBe(10)
+            expect(fail({ enhancement: 20, durability: 20 }).durability).toBe(19)
+        })
+    })
+
+
+})
